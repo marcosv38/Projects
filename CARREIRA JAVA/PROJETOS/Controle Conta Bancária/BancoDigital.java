178cdc4c39e0cd75444b0e71812a1959c931d41d
marcosv38 <50200539+marcosv38@users.noreply.github.com>
2023-12-09 16:54:51 -0300

    Update BancoDigital.java

--- CARREIRA JAVA/PROJETOS/Controle Conta Bancária/BancoDigital.java
+++ CARREIRA JAVA/PROJETOS/Controle Conta Bancária/BancoDigital.java
@@ -8,8 +8,8 @@ public class BancoDigital {
         //Variáveis
         String nomeCliente = "";
         String tipoDeConta = "";
-        double saldoDaConta = 0;
-        int opcoesSistema = 0;
+        double saldoDaConta = 20;
+        int opcoesSistema = 20;
 
         //Mensagens de inicialização
         System.out.println("Iniciando sistema...");
