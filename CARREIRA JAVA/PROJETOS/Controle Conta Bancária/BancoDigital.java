bb4aec38eced194a1e827c6a4214d6c18e9ba900
Marcos <marcos.bwr@gmail.com>
2023-12-09 15:15:30 -0300

    Finalizado

--- CARREIRA JAVA/PROJETOS/Controle Conta Bancária/BancoDigital.java
+++ CARREIRA JAVA/PROJETOS/Controle Conta Bancária/BancoDigital.java
@@ -22,7 +22,7 @@ public class BancoDigital {
 
                 //Dando a opção de cadastrar o cliente
                 System.out.println("Não há clientes cadastrados.");
-                System.out.println("Você quer cadastrar um novo cliente? S ou N?");
+                System.out.println("Você deseja cadastrar um novo cliente? S ou N?");
                 String decisao = leituraTeclado.nextLine();
 
                 if(decisao.equalsIgnoreCase("S")){
@@ -33,7 +33,7 @@ public class BancoDigital {
                     tipoDeConta = leituraTeclado.nextLine();
                     System.out.println("Ótimo! para começar qual seria o saldo atual da conta:");
                     saldoDaConta = leituraTeclado.nextDouble();
-                    System.out.println("Perfeito! Agora podemos iniciar o sistema com a configuração adequada a você. Bem vindo!!");
+                    System.out.println("Perfeito! Agora podemos iniciar o sistema com a configuração adequada a você. Muito obrigado!!");
 
                 } else if (decisao.equalsIgnoreCase("N")) {
                     //Encerrando o programa caso o cliente não queira se cadastrar e usar o sistema
@@ -48,26 +48,143 @@ public class BancoDigital {
 
         }
 
+        if(!nomeCliente.equalsIgnoreCase("")) {
 
-        while(!nomeCliente.equalsIgnoreCase("")){
-            if ()
-            System.out.println("Dados carregados... Bem vindo "+ nomeCliente);
+            System.out.println("""
+            Carregando dados... 
+            
+            Bem vindo      """ + nomeCliente);
+
             System.out.println("");
 
             System.out.println("""
-                        ***************************************
-                        
-                        Nome do cliente: """+nomeCliente+"""
-                        
-                        Tipo de conta: """+tipoDeConta+"""
-                        
-                        Saldo da conta: """+saldoDaConta+"""
-                        
-                        ***************************************
+                    ***************************************
+
+                    Dados do cliente:
+                                            
+                    Nome do cliente: """ + nomeCliente + """
+                                            
+                    Tipo de conta: """ + tipoDeConta + """
+                                            
+                    Saldo da conta: """ + saldoDaConta + """
+                                            
+                    ***************************************
+                    """);
+
+
+            while (opcoesSistema != 4) {
+
+                System.out.println("""
+                                                
+                        Operações:
+                        1- Consultar Saldo
+                        2- Receber valor
+                        3- Transferir valor
+                        4- Sair
+                                                
+                        Digite a opção desejada:
+                                                
                         """);
 
+                opcoesSistema = leituraTeclado.nextInt();
+
+                switch (opcoesSistema) {
+
+                    case 1:
+                        System.out.println("Seu saldo atual é: R$ " + saldoDaConta);
+                        break;
+                    case 2:
+                        //bloco de código sem a utilização do método Depositar()
+                        /*System.out.println("Quanto você pretende depositar:");
+                        saldoDaConta += leituraTeclado.nextInt();
+                        System.out.println("Muito bem... Depositando...");
+                        System.out.println("Prontinho! Seu novo saldo é: R$ " + saldoDaConta);
+                        */
+
+                        saldoDaConta = Depositar(saldoDaConta);
+
+                        break;
+
+                    case 3:
+                        //bloco de código sem a utilização do método fazerTransferencia()
+                       /* System.out.println("Quanto você pretende transferir:");
+                        double valorTransferencia = leituraTeclado.nextDouble();
+
+                        if (valorTransferencia < saldoDaConta) {
+                            saldoDaConta -= valorTransferencia;
+                            System.out.println("Fazendo transferência... Só um momento...");
+                            System.out.println("""
+
+                                    Transferência concluída!! Segue os dados da transferência:
+
+                                    Valor da transferência: R$ """ + valorTransferencia + """
+
+                                    Saldo antes da transferência: R$ """ + (saldoDaConta + valorTransferencia) + """
+
+                                    Saldo Atual: R$ """ + saldoDaConta + """
+
+                                    """);
+
+                        } else if (valorTransferencia > saldoDaConta) {
+                            System.out.println("Não é possível realizar a transferência... Saldo insuficiente.");
+                        }*/
+
+                        saldoDaConta = FazerTransferencia(saldoDaConta);
+
+                        break;
+                    case 4:
+                        System.out.println("Muito obrigado por usar nosso sistema. Até logo!!");
+                        break;
+                    default:
+                        break;
+
+                }
+
+
+            }
         }
 
 
+
     }
+
+    public static double FazerTransferencia(double saldo){
+        System.out.println("Quanto você pretende transferir:");
+        Scanner leituraDoTeclado = new Scanner(System.in);
+        double valorTransferencia = leituraDoTeclado.nextDouble();
+
+        if (valorTransferencia < saldo) {
+            saldo -= valorTransferencia;
+            System.out.println("Fazendo transferência... Só um momento...");
+            System.out.println("""
+                                                                
+                                    Transferência concluída!! Segue os dados da transferência: 
+                                                                
+                                    Valor da transferência: R$ """ + valorTransferencia + """
+                                                                
+                                    Saldo antes da transferência: R$ """ + (saldo + valorTransferencia) + """
+                                                                
+                                    Saldo Atual: R$ """ + saldo + """
+
+                                    """);
+
+        } else if (valorTransferencia > saldo) {
+            System.out.println("Não é possível realizar a transferência... Saldo insuficiente.");
+        }
+
+        return saldo;
+    }
+
+    public static double Depositar (double saldo){
+        Scanner leituraTeclado = new Scanner(System.in);
+        System.out.println("Quanto você pretende depositar:");
+        saldo += leituraTeclado.nextInt();
+        System.out.println("Muito bem... Depositando...");
+        System.out.println("Prontinho! Seu novo saldo é: R$ " + saldo);
+
+        return saldo;
+
+    }
+
+
 }
